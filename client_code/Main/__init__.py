03304c7e283e3e29b4440824dcb62cf4beec1719
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-04 14:31:08 +0000

    Saved version

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import MainTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 from ..Result import Result
 
