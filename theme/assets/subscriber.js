2489176234fdb70e578860346bea0337239af3f5
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-04 12:50:15 +0000

    Saved version

--- theme/assets/subscriber.js
+++ theme/assets/subscriber.js
@@ -1,3 +1,15 @@
-function hello() {
-  console.log("hello");
-}
+var connection = new autobahn.Connection({
+   url: 'ws://127.0.0.1:8080/ws',
+   realm: 'speedtest'}
+);
+
+connection.onopen = function (session) {
+
+   function onResult(args) {
+      console.log("Got event:", args[0]);
+   }
+
+   session.subscribe('results', onResult);
+};
+
+connection.open();
