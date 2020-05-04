46bd45691eee782837500c921e73bd798ba59bc0
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-04 13:27:47 +0000

    Saved version

--- client_code/Result/__init__.py
+++ client_code/Result/__init__.py
@@ -1,9 +1,6 @@
 from ._anvil_designer import ResultTemplate
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 class Result(ResultTemplate):
   def __init__(self, **properties):
