2f550d03f018b2bd3b9805c557077310cb2fa55a
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-04 12:09:50 +0000

    Saved automatically

--- client_code/Result/__init__.py
+++ client_code/Result/__init__.py
@@ -1,16 +1,13 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ResultTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
-class Form1(Form1Template):
-
+class Result(ResultTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-
-
