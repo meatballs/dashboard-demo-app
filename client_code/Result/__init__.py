be4584d3d05f5953a553c05bb4b1d02abb5fbc77
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-05 09:01:23 +0000

    Saved version

--- client_code/Result/__init__.py
+++ client_code/Result/__init__.py
@@ -5,7 +5,11 @@ import anvil.server
 class Result(ResultTemplate):
   def __init__(self, **properties):
     self.subscriber.set_event_handler("x-download-speed-updated", self.refresh_download_speed)
+    self.subscriber.set_event_handler("x-cpu-percent-updated", self.refresh_cpu_percent)
     self.init_components(**properties)
 
   def refresh_download_speed(self, download, timestamp, **event_args):
     self.item["download_speed"] = download
+    
+  def refresh_cpu_percent(self, cpu_percent, **event_args):
+    self.item["cpu_percent"] = cpu_percent
