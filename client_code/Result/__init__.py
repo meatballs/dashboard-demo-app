19d20e48c1f68d5f0face816a35b1f6646b636f0
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-05 09:48:27 +0000

    Saved version

--- client_code/Result/__init__.py
+++ client_code/Result/__init__.py
@@ -9,7 +9,8 @@ class Result(ResultTemplate):
     self.init_components(**properties)
 
   def refresh_download_speed(self, download_speed, timestamp, **event_args):
-    self.item["download_speed"] = download_speed
+    display_speed = download_speed / 10 ** 6
+    self.item["download_speed"] = f"{display_speed:.2f} Mbps"
     self.refresh_data_bindings()
     
   def refresh_cpu_percent(self, cpu_percent, **event_args):
