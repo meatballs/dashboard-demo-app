4702377f4643b39455ef1e9c50119f982cbe2395
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2020-05-05 13:46:35 +0000

    Saved version

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -7,7 +7,7 @@ class Dashboard(DashboardTemplate):
     self.subscriber.set_event_handler("x-download-speed-updated", self.refresh_download_speed)
     self.subscriber.set_event_handler("x-cpu-percent-updated", self.refresh_cpu_percent)
     self.cpu_percent_plot.layout.xaxis.range = [0, 100]
-    self.cpu_percent_plot.layout.
+    self.cpu_percent_plot.layout.height = 200
     self.init_components(**properties)
 
   def refresh_download_speed(self, download_speed, timestamp, **event_args):
